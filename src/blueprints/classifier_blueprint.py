3e6ce18e9d75441bc3d94e5b9ce8905c575ec87d
Joni Seppälä <joni.seppala@tuni.fi>
2021-03-06 14:51:12 +0200

    Fix get_q method. iIncrease max_epochs.

--- src/blueprints/classifier_blueprint.py
+++ src/blueprints/classifier_blueprint.py
@@ -108,10 +108,10 @@ class ClassifierNetwork(vis.summarizable_module.SummarizableModule):
         assert len(x.shape) == 4 and x.shape[0] == 1, x.shape
         with torch.no_grad():
             q_pred = self.forward(x).q_logits
-            import pdb;pdb.set_trace()
             # _, predicted = torch.max(q_logits, 1)
             # return predicted.item() + self.config_clf.first_class
-            return q_pred + self.config_clf.first_class
+            q_pred = q_pred.round()
+            return q_pred.item() + self.config_clf.first_class
 
     def forward(self, x):
         self.summarizer.register_images('train', {'input': x}, normalize=True, only_once=True)
